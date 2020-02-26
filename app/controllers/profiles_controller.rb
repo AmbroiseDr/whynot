dd24c03c0b470a022d6a6b962e1ad939f939c75f
sybileduarte <sybile.duarte@edhec.com>
2020-02-26 15:01:51 +0100

    Created routes, matches controller, invitation controller"

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,8 @@
 #act as user controller
 class ProfilesController < ApplicationController
   private
+
   def article_params
-  params.require(:profile).permit(:photo)
-end
+    params.require(:profile).permit(:photo)
+  end
 end
