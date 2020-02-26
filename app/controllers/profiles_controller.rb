394597d5171317e1a6965ce859813e1048b03cb4
pauline jouve <pauline.jouve@edhec.com>
2020-02-26 15:30:31 +0100

    taggable setup + js + scss

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@
 class ProfilesController < ApplicationController
   private
   def article_params
-  params.require(:profile).permit(:photo)
+  params.require(:profile).permit(:photo, :tag_list)
 end
 end
