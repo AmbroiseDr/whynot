5b866017ac765e6661b95631780057d7e4472607
DanCema <dan.cemachovic@edhec.com>
2020-02-27 13:37:45 +0100

    search movie done

--- app/javascript/components/search_movie.js
+++ app/javascript/components/search_movie.js
@@ -1,23 +1,77 @@
-
-const resultMovies = (event) => {
-  const query = event.currentTarget.value
-  const omdbKey = event.currentTarget.dataset.omdb
-  fetch(`http://www.omdbapi.com/?apikey=${omdbKey}&s=${query}`)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data); // Look at local_names.default
-    });
-}
-
+import autoComplete from "../plugins/autoComplete";
 
 
 const searchMovie = () => {
-  const inputMovie = document.getElementById("search-movie")
-  if (inputMovie) {
-    inputMovie.addEventListener("keyup",(event) => {
-      resultMovies(event)
-    })
+  const input = document.querySelector("#autoComplete")
+  if (input){
+    new autoComplete({
+        data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+          src: async () => {
+            // API key token
+            const token = document.querySelector("#autoComplete").dataset.omdb;
+            // User search query
+            const query = document.querySelector("#autoComplete").value;
+            // Fetch External Data Source
+            const source = await fetch(`http://www.omdbapi.com/?apikey=${token}&s=${query}`);
+            // Format data into JSON
+            const data = await source.json();
+            // Return Fetched data
+            console.log(data.Search)
+            return data.Search;
+          },
+          key: ["Title"],
+          cache: false
+        },
+        sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
+            if (a.match < b.match) return -1;
+            if (a.match > b.match) return 1;
+            return 0;
+        },
+        selector: "#autoComplete",           // Input field selector              | (Optional)
+        threshold: 3,                        // Min. Chars length to start Engine | (Optional)
+        debounce: 300,                       // Post duration for engine to start | (Optional)
+        searchEngine: "strict",              // Search Engine type/mode           | (Optional)
+        resultsList: {                       // Rendered results list object      | (Optional)
+            render: true,
+            container: source => {
+                source.setAttribute("id", "movie_list");
+            },
+            destination: document.querySelector("#search-movie"),
+            position: "beforeend",
+            element: "ul"
+        },
+        maxResults: 5,                         // Max. number of rendered results | (Optional)
+        highlight: true,                       // Highlight matching results      | (Optional)
+        resultItem: {                          // Rendered result item            | (Optional)
+            content: (data, source) => {
+                source.innerHTML = data.match;
+            },
+            element: "li"
+        },
+        onSelection: feedback => {
+          const selection = feedback.selection.value;
+          // Render selected choice to selection div
+          document.querySelector(".selection").innerHTML = `<img src='${selection.Poster}'>
+          <div>
+            <div class='card-title'>
+              ${selection.Title}
+            </div>
+            <div class='card-text'>
+              ${selection.Year}
+            </div>
+          </div>`;
+          // Clear Input
+          document.querySelector("#autoComplete").value = "";
+          // Change placeholder with the selected value
+          document
+            .querySelector("#autoComplete")
+            .setAttribute("placeholder", selection.Title);
+          // Concole log autoComplete data feedback
+        }
+    });
   }
 }
 
 export { searchMovie }
+
+
