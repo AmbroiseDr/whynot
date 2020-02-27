76ac95b24569912316954fbfcd3e35b21ff6319b
Ambroise Drogou <ambroise.drogou@edhec.com>
2020-02-27 14:49:47 +0100

    I have implemented the map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,12 @@
 class MapsController < ApplicationController
+  def index
+    @markers =
+      [{
+        lat: 48.8534,
+        lng: 2.3488,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+        # Uncomment the above line if you want each of your markers to display a info window when clicked
+        # (you will also need to create the partial "/flats/map_box")
+      }]
+  end
 end
