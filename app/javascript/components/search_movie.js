f8a58e780e414cfc405ef18a1f11b71f47874aab
DanCema <dan.cemachovic@edhec.com>
2020-02-27 11:37:56 +0100

    search movie backend

--- app/javascript/components/search_movie.js
+++ app/javascript/components/search_movie.js
@@ -1,1 +1,23 @@
-search_movie.js
+
+const resultMovies = (event) => {
+  const query = event.currentTarget.value
+  const omdbKey = event.currentTarget.dataset.omdb
+  fetch(`http://www.omdbapi.com/?apikey=${omdbKey}&s=${query}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data); // Look at local_names.default
+    });
+}
+
+
+
+const searchMovie = () => {
+  const inputMovie = document.getElementById("search-movie")
+  if (inputMovie) {
+    inputMovie.addEventListener("keyup",(event) => {
+      resultMovies(event)
+    })
+  }
+}
+
+export { searchMovie }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,7 @@ import { clickTags } from '../components/form_profile_activities_clickable_tags'
 
 
 clickTags();
+
+
+import { searchMovie } from '../components/search_movie';
+searchMovie();
