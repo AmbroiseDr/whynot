5ba125342ff6e5b4764ab15816103fca28741de7
Ambroise Drogou <ambroise.drogou@edhec.com>
2020-03-02 16:27:00 +0100

    channel ok, receive message & send ok, img ok

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,18 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: "User"
 
   validates :content, length: { maximum: 100  }
+
+
+  after_create :broadcast_message
+
+  def broadcast_message
+
+    chat_room_id = self.sender.id * self.receiver.id
+
+    ActionCable.server.broadcast("chat_room_#{chat_room_id}", {
+      message: self.content,
+      sender: sender.id,
+      img: sender.photo.key
+    })
+  end
 end
