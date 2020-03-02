82cffaa4fd7c1439ac0794310b267f55cfb29bef
Ambroise Drogou <ambroise.drogou@edhec.com>
2020-03-02 16:50:51 +0100

    quick fix again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://www.whynt.live/cable'
-  config.action_cable.allowed_request_origins = [ 'http://www.whynt.live', 'https://www.whynt.live' ]
+  # config.action_cable.url = 'wss://www.whynt.live/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://www.whynt.live', 'https://www.whynt.live' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
