475c7f0059548932c83b2a454c04f330064f1260
Ambroise Drogou <ambroise.drogou@edhec.com>
2020-03-03 17:59:44 +0100

    some changes

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -13,13 +13,11 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   // const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const map = new google.maps.Map(document.getElementById('map'), {center : {lat: 48.8522428, lng: 2.3393369}, zoom: 13});
   const markers = JSON.parse(oldMarkers);
-
-
-  // markers.push({
-  //   lat: 48.8522428,
-  //   lng: 2.3393369,
-  //   icon:'https://res.cloudinary.com/ambroise/image/upload/c_scale,w_22/v1582822068/logo_why_not_-_copie_zsernr.png',
-  // });
+  const userMarker = new google.maps.Marker({position: {lat: 48.8522428,lng: 2.3393369},
+    map: map,
+    title: "test",
+    animation: google.maps.Animation.DROP,
+    icon:'https://res.cloudinary.com/ambroise/image/upload/c_scale,w_22/v1582822068/logo_why_not_-_copie_zsernr.png'});
   let counter = 1;
 
 
