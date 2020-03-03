900edae89ca5c93de99d5c667402ec2bf776f6e8
Ambroise Drogou <ambroise.drogou@edhec.com>
2020-03-03 17:29:34 +0100

    add infowindows to the map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -10,38 +10,33 @@ const mapElement = document.getElementById('map');
 
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const oldMarkers = mapElement.dataset.markers
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  // const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new google.maps.Map(document.getElementById('map'), {center : {lat: 48.8522428, lng: 2.3393369}, zoom: 13});
   const markers = JSON.parse(oldMarkers);
-  markers.push({
-    lat: 48.8522428,
-    lng: 2.3393369,
-    icon:'https://res.cloudinary.com/ambroise/image/upload/c_scale,w_22/v1582822068/logo_why_not_-_copie_zsernr.png',
-  });
-  let counter = 1;
-  markers.forEach((marker)=>{
-    marker.animation = google.maps.Animation.DROP
-    counter = counter + 1
-    window.setTimeout(function() {
-      map.addMarkers([marker]);
-    }, 200 * counter);
-    let infowindow = new google.maps.InfoWindow({
-      content: "contentString"
-    });
-    console.log(infowindow);
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-    });
-  })
 
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(48.8522428, 2.3393369);
-    map.setZoom(14);
-  } else {
-    map.setCenter(48.8522428, 2.3393369);
-    map.setZoom(14);
-    // map.fitLatLngBounds(markers);
-  }
+
+  // markers.push({
+  //   lat: 48.8522428,
+  //   lng: 2.3393369,
+  //   icon:'https://res.cloudinary.com/ambroise/image/upload/c_scale,w_22/v1582822068/logo_why_not_-_copie_zsernr.png',
+  // });
+  let counter = 1;
+
+
+  markers.forEach((marker) => {
+    let infowindow = new google.maps.InfoWindow({
+      content: `
+        <h1>${marker.name}</h1>
+        <a href="/matches/15">Profile</a>
+      `
+    });
+    window.setTimeout(function() {
+      const newMarker = new google.maps.Marker({position: marker, map: map, title: "test",animation: google.maps.Animation.DROP});
+      newMarker.addListener('click', function() {
+          infowindow.open(map, newMarker);
+      });
+    }, counter * 200);
+    counter = counter + 1;
+  })
 }
 
