494b33d609fc352cc4f6a3f9bce6a4c8c1f9276d
sybileduarte <sybile.duarte@edhec.com>
2020-03-05 14:34:19 +0100

    corrected bugs

--- app/javascript/components/jauge.js
+++ app/javascript/components/jauge.js
@@ -1,20 +1,18 @@
 
 const AnimateJauge = () => {
+  const elem = document.querySelector(".progress-bar");
+  if(elem) {
+      let width = -1;
+      const id = setInterval(frame, 10);
+      function frame() {
 
-const elem = document.querySelector(".progress-bar");
-if(elem){
-    let width = -1;
-    const id = setInterval(frame, 10);
-    function frame() {
-
-      if (width >= elem.dataset.percentage) {
-        clearInterval(id);
-      } else {
-        width++;
-        console.log("heyyyy")
-        elem.style.width = width + "%";
+        if (width >= elem.dataset.percentage) {
+          clearInterval(id);
+        } else {
+          width++;
+          elem.style.width = width + "%";
+        }
       }
     }
-  }
 }
 export { AnimateJauge }
