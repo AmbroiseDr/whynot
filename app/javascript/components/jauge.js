4c15b4e0dc286e6a99bea17fe6f0b40e61d5a498
pauline jouve <pauline.jouve@edhec.com>
2020-03-05 12:37:52 +0100

    filljauge

---
+++ app/javascript/components/jauge.js
@@ -0,0 +1,20 @@
+
+const AnimateJauge = () => {
+
+const elem = document.querySelector(".progress-bar");
+if(elem){
+    let width = -1;
+    const id = setInterval(frame, 10);
+    function frame() {
+
+      if (width >= elem.dataset.percentage) {
+        clearInterval(id);
+      } else {
+        width++;
+        console.log("heyyyy")
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+export { AnimateJauge }
